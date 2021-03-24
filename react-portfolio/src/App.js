0007115352f7e5b321916cd35d316b30639e26b3
Mengmei Tu <redmmeii@gmail.com>
2021-03-24 13:47:30 -0500

    add nav route

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from '../src/pages/Home';
+import About from '../src/pages/About';
+import Projects from '../src/pages/Projects';
+import Contact from '../src/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        {/* setup router */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+
+        <div className="navigation">
+          {/* <img src={logo} className="logo" alt="Logo" /> */}
+          <div className="navigation-sub">
+
+            {/* setup links */}
+            <Link to="/" className="item">Home</Link>
+            <Link to="/about" className="item">About</Link>
+            <Link to="/projects" className="item">Projects</Link>
+            <Link to="/contact" className="item">Contact</Link>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
